4909b7409577d4ed04fcd0c390f62f432601ce5a
sensei38 <shihowdima1337@gmail.com>
2026-05-27 18:02:15 +0800

    created new function load_all_levels which is loading all levels from jsonfiles

--- src/word_manager.py
+++ src/word_manager.py
@@ -7,4 +7,12 @@ class WordManager:
         self.levels = []
         self.load_all_levels()
         
+    def load_all_levels(self):
+        if not os.path.exists(self.levels_folder):
+            self.create_default_levels()
+            
+        for filename in sorted(os.listdir(self.levels_folder)):
+            if filename.endswith(".json"):
+                with open(os.path.join(self.levels_folder, filename), 'r', encoding='utf-8') as f:
+                    self.levels.append(json.load(f))
    
